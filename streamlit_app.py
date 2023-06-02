23a6208e43c3f1b3e0311d91442782ca2f3bd38a
devxkitchen <135175941+devxkitchen@users.noreply.github.com>
2023-06-02 16:25:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,6 @@ try:
 except URLError as e:
     streamlit.error()
 
-# don't run anything past here while we troubleshoot
-#streamlit.stop()
 #my_cur = my_cnx.cursor()
 #my_cur.execute("select * from fruit_load_list")
 #my_data_rows = my_cur.fetchall()
@@ -62,6 +60,9 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+  
 # Allow the end user to add a fruit to the list
 # streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
